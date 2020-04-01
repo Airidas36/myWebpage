37b8d0ce430e4524a87c769bb6c676070fe6f5e5
Airidas36 <50202461+Airidas36@users.noreply.github.com>
2020-04-01 20:35:57 +0300

    Added active nav-item class asignment/removal with js, signup modal image, fixed the navbar and carousel responsive issues.
    TODO: Finish the singup from, make a template of the navbar(? not sure yet, maybe will be using another indentically looking navbar for the rest of the pages, because of the issues with carousel).

--- includes/script.js
+++ includes/script.js
@@ -1,30 +1,37 @@
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    document.getElementById('loginmodalbutton').style.margin='103px 0px 0px 5.8px';
-                }
-                form.classList.add('was-validated');
-            }, false);
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        document.getElementById('loginmodalbutton').style.margin='103px 0px 0px 5.8px';
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+
+    $(function() {
+        $('#loginmodalbutton').hover(function() {
+        $('#loginbody').css('backdrop-filter','blur(0px) grayscale(0%)');
+        $('#loginbody').css('-webkit-backdrop-filter','blur(0px) grayscale(0%)');
+
+        }, function() {
+        // on mouseout, reset the background colour
+        $('#loginbody').css('backdrop-filter','blur(5px) grayscale(100%)');
+        $('#loginbody').css('-webkit-backdrop-filter','blur(5px) grayscale(100%)');
+
         });
-    }, false);
-})();
-
-$(function() {
-    $('#loginmodalbutton').hover(function() {
-      $('.modal-body').css('backdrop-filter', 'blur(0px) grayscale(0%)');
-      $('.modal-body').css('-webkit-backdrop-filter', 'blur(0px) grayscale(0%)');
-
-    }, function() {
-      // on mouseout, reset the background colour
-      $('.modal-body').css('backdrop-filter', 'blur(5px) grayscale(100%)');
-      $('.modal-body').css('-webkit-backdrop-filter', 'blur(5px) grayscale(100%)');
-
     });
-  });
+
+    $(document).ready(function() {
+        $(".nav-item").click(function () {
+            $(".nav-item").removeClass("active");
+            $(this).addClass("active");     
+        });
+    });
